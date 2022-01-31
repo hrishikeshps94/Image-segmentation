0c4cd8dd0ddf94eff55d119cb7a5157815d935eb
hrishikeshps94 <hrishikeshps94@gmail.com>
2022-02-01 00:48:07 +0530

    val step update

--- train.py
+++ train.py
@@ -110,7 +110,15 @@ def train_step(batch_data,model,optimizer,lr_scheduler, loss_opts):
 
 def valid_step(batch_data,model, loss_opts):
     ####
-    # model = run_info["net"]["desc"]
+    loss_func_dict = {
+        "bce": cross_entropy_loss,
+        "dice": dice_loss,
+        "mse": mse_loss,
+        "msge": msge_loss,
+    }
+    result_dict = {"Scalar": {}}
+    # use 'ema' to add for EMA calculation, must be scalar!
+    track_value = lambda name, value: result_dict["Scalar"].update({name: value})
     model.eval()  # infer mode
 
     ####
@@ -122,42 +130,68 @@ def valid_step(batch_data,model, loss_opts):
     imgs_gpu = imgs_gpu.permute(0, 3, 1, 2).contiguous()
 
     # HWC
-    true_np = torch.squeeze(true_np).to(device).type(torch.int64)
-    true_hv = torch.squeeze(true_hv).to(device).type(torch.float32)
+    true_np = true_np.to(device).type(torch.int64)
+    true_hv = true_hv.to(device).type(torch.float32)
+    disp_true_np = torch.squeeze(batch_data["np_map"]).to(device).type(torch.int64)
+    disp_true_hv = torch.squeeze(batch_data["hv_map"]).to(device).type(torch.float32)
+    true_np_onehot = (F.one_hot(true_np, num_classes=2)).type(torch.float32)
 
+    disp_true_dict = {
+        "np": disp_true_np,
+        "hv": disp_true_hv,
+    }
     true_dict = {
-        "np": true_np,
+        "np": true_np_onehot,
         "hv": true_hv,
     }
 
     if model.nr_types is not None:
         true_tp = batch_data["tp_map"]
         true_tp = torch.squeeze(true_tp).to(device).type(torch.int64)
-        true_dict["tp"] = true_tp
+        disp_true_dict["tp"]=true_tp
+        true_tp_onehot = F.one_hot(true_tp, num_classes=model.nr_types)
+        true_tp_onehot = true_tp_onehot.type(torch.float32)
+        true_dict["tp"] = true_tp_onehot
 
     # --------------------------------------------------------------
     with torch.no_grad():  # dont compute gradient
-        pred_dict = model(imgs_gpu)
+        org_dict = model(imgs_gpu)
         pred_dict = OrderedDict(
-            [[k, v.permute(0, 2, 3, 1).contiguous()] for k, v in pred_dict.items()]
+        [[k, v.permute(0, 2, 3, 1).contiguous()] for k, v in org_dict.items()] )       
+        disp_pred_dict = OrderedDict(
+            [[k, v.permute(0, 2, 3, 1).contiguous()] for k, v in org_dict.items()]
         )
-        pred_dict["np"] = F.softmax(pred_dict["np"], dim=-1)[..., 1]
+        pred_dict["np"] = F.softmax(pred_dict["np"], dim=-1)
+        disp_pred_dict["np"] = F.softmax(disp_pred_dict["np"], dim=-1)[..., 1]
         if model.nr_types is not None:
-            type_map = F.softmax(pred_dict["tp"], dim=-1)
+            pred_dict["tp"] = F.softmax(pred_dict["tp"], dim=-1)
+            type_map = F.softmax(disp_pred_dict["tp"], dim=-1)
             type_map = torch.argmax(type_map, dim=-1, keepdim=False)
             type_map = type_map.type(torch.float32)
-            pred_dict["tp"] = type_map
+            disp_pred_dict["tp"] = type_map
+        ####
+        loss = 0
+        # loss_opts = run_info["net"]["extra_info"]["loss"]
+        for branch_name in pred_dict.keys():
+            for loss_name, loss_weight in loss_opts[branch_name].items():
+                loss_func = loss_func_dict[loss_name]
+                loss_args = [true_dict[branch_name], pred_dict[branch_name]]
+                if loss_name == "msge":
+                    loss_args.append(true_np_onehot[..., 1])
+                term_loss = loss_func(*loss_args)
+                track_value("loss_%s_%s" % (branch_name, loss_name), term_loss.cpu().item())
+                loss += loss_weight * term_loss
+
+        track_value("overall_loss", loss.cpu().item())
     # * Its up to user to define the protocol to process the raw output per step!
-    result_dict = {  # protocol for contents exchange within `raw`
-        "raw": {
+    result_dict['raw'] = {  # protocol for contents exchange within `raw` {
             "imgs": imgs.numpy(),
-            "true_np": true_dict["np"].cpu().numpy(),
-            "true_hv": true_dict["hv"].cpu().numpy(),
-            "prob_np": pred_dict["np"].cpu().numpy(),
-            "pred_hv": pred_dict["hv"].cpu().numpy(),
+            "true_np": disp_true_dict["np"].cpu().numpy(),
+            "true_hv": disp_true_dict["hv"].cpu().numpy(),
+            "prob_np": disp_pred_dict["np"].cpu().numpy(),
+            "pred_hv": disp_pred_dict["hv"].cpu().numpy(),
         }
-    }
     if model.nr_types is not None:
-        result_dict["raw"]["true_tp"] = true_dict["tp"].cpu().numpy()
-        result_dict["raw"]["pred_tp"] = pred_dict["tp"].cpu().numpy()
+        result_dict["raw"]["true_tp"] = disp_true_dict["tp"].cpu().numpy()
+        result_dict["raw"]["pred_tp"] = disp_pred_dict["tp"].cpu().numpy()
     return result_dict
